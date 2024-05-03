01d900b0911de38adf1fb4cca061bc3508cf3b56
William Ndirangu <wwagura0@gmail.com>
2024-05-03 12:18:55 +0300

    Add

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -3,9 +3,14 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Article from "../components/Article";
+import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
